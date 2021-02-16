bff0ae0236fa69ed745863b5c52b68a9b9364444
oliver <up824985@myport.ac.uk>
2021-02-16 16:49:56 +0000

    Added level change
    Added level change

--- PROJECT/GAME/GAME_V_0_1/ARCADE3_V_0_1/Assets/MainMenu.cs
+++ PROJECT/GAME/GAME_V_0_1/ARCADE3_V_0_1/Assets/MainMenu.cs
@@ -1,9 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class MainMenu : MonoBehaviour
 {
+    bool CoinLoaded = false;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +16,14 @@ public class MainMenu : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        if (Input.anyKey)
+        {
+            CoinLoaded = true;
+            //Change UI
+            SceneManager.LoadScene(1);
+        }
+
+
     }
 }
